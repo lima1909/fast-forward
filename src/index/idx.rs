7ca2a92a395aa0b49186f504985ea3a69ae35ebc
lima1909 <linke.mario@web.de>
2023-03-19 14:04:23 +0100

    remove method len from Index trait and add more teste for mulit index

--- src/index/idx.rs
+++ src/index/idx.rs
@@ -2,14 +2,17 @@ use std::{fmt::Debug, marker::PhantomData};
 
 use crate::{error::Error, Idx, Result};
 
-#[allow(clippy::len_without_is_empty)]
+/// This trait descripe the interface for an possible Index.
 pub trait Index: Debug {
+    /// Create a new `Index`.
     fn new(i: Idx) -> Self;
+    /// Add an `Index`.
     fn add(&mut self, i: Idx) -> Result;
+    /// Get all `Indecies`. **Importand:** The items must be sorted in ascending order!
     fn get(&self) -> &[Idx];
-    fn len(&self) -> usize;
 }
 
+/// A unique `Index`. This means, it contains only one `Index` for a given `Key`.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct Unique([Idx; 1]);
 
@@ -28,13 +31,9 @@ impl Index for Unique {
     fn get(&self) -> &[Idx] {
         &self.0
     }
-
-    #[inline]
-    fn len(&self) -> usize {
-        1
-    }
 }
 
+/// A multi `Index`. This means, it contains many `Indices` for a given `Key`.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct Multi(Vec<Idx>);
 
@@ -55,11 +54,6 @@ impl Index for Multi {
     #[inline]
     fn get(&self) -> &[Idx] {
         &self.0
-    }
-
-    #[inline]
-    fn len(&self) -> usize {
-        self.0.len()
     }
 }
 
@@ -97,7 +91,7 @@ mod tests {
         let err = u.add(1);
         assert!(err.is_err());
 
-        assert_eq!(1, u.len());
+        assert_eq!(1, u.get().len());
     }
 
     #[test]
@@ -106,7 +100,30 @@ mod tests {
         assert_eq!(&[0], m.get());
 
         m.add(1).unwrap();
-        assert_eq!(2, m.len());
+        assert_eq!(2, m.get().len());
         assert_eq!(&[0, 1], m.get());
+    }
+
+    #[test]
+    fn multi_duplicate() {
+        let mut m = Multi::new(1);
+        assert_eq!(&[1], m.get());
+
+        // ignore add: 1, 1 exists already
+        m.add(1).unwrap();
+        assert_eq!(1, m.get().len());
+        assert_eq!(&[1], m.get());
+    }
+
+    #[test]
+    fn multi_ordered() {
+        let mut m = Multi::new(5);
+        assert_eq!(&[5], m.get());
+
+        m.add(3).unwrap();
+        m.add(1).unwrap();
+        m.add(4).unwrap();
+
+        assert_eq!(&[1, 3, 4, 5], m.get());
     }
 }
