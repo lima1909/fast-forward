397a6cc27c9b8564f6f3138fafd099e4f4b13d7a
lima1909 <linke.mario@web.de>
2023-04-12 20:21:07 +0200

    add test for filtering list with deleted Item

--- src/list.rs
+++ src/list.rs
@@ -336,6 +336,18 @@ mod tests {
     }
 
     #[test]
+    fn iter_filter_delete() {
+        let mut l: List<_> = vec![1, 2, 3].into();
+        l.delete(1, |_, _| {});
+
+        let mut it = l.filter(Cow::Owned(vec![0, 1, 2]));
+
+        assert_eq!(Some(&1), it.next());
+        assert_eq!(Some(&3), it.next());
+        assert_eq!(None, it.next());
+    }
+
+    #[test]
     fn filter_first() {
         let l: List<_> = vec![1, 2, 3].into();
         let mut it = l.filter(Cow::Owned(vec![0, 1]));
